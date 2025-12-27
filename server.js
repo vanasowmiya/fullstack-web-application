de6cec99d59f85180b6a2d5dbb0f51a0817bb113
vanasowmiya <vanasowmiya@gmail.com>
2025-12-27 12:24:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+ const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
